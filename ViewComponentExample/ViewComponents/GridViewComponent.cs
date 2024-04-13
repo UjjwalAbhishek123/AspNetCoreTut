a9e0439c8b99d6f2479720d5ad4c8ba64ab6b19d
Ujjwal Abhishek <ujjwalabhishek15@gmail.com>
2024-04-13 14:25:35 +0530

    Ninth Commit

---
+++ ViewComponentExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentExample.ViewComponents
+{
+    public class GridViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //invoked as Partial View & PartialView name is not Default
+            return View("Sample");
+
+            //When Default.cshtml was name -> return View();
+            //invoked as Partial View
+            //Location is -> Views/Shared/Components/Grid/Default.cshtml
+        }
+    }
+}
