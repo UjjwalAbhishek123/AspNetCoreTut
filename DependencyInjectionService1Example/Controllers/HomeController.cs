a4cba9398845118966833c4ad94ad8a95738acd3
Ujjwal Abhishek <ujjwalabhishek15@gmail.com>
2024-08-14 11:55:08 +0530

    14th commit DependencyInjectionExample1

---
+++ DependencyInjectionService1Example/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using DepnInjcService1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjectionService1Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        //create a reference variable for CitiesService.cs
+        //making it readonly avoids the unwanted assignment of unwanted values into the field
+        private readonly CitiesService _citiesService;
+
+        //constructor for object creation of CitiesService class
+        public HomeController()
+        {
+            //create object of CitiesService class
+            _citiesService = new CitiesService(); 
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //Calling Service Method
+            List<string> cities = _citiesService.GetCities();
+
+            return View(cities);
+        }
+    }
+}
---
+++ DepnInjcService1/CitiesService.cs
@@ -0,0 +1,24 @@
+﻿namespace DepnInjcService1
+{
+    //Goal -> at the time of loading the View, we have to displaya list of Cities that are retrieved from business logic
+    public class CitiesService
+    {
+        //Adding code to send data to controller
+        private List<string> _cities;
+
+        //initializing the property in constructor
+        public CitiesService()
+        {
+            _cities = new List<string>()
+            {
+                "London", "Paris", "Tokyo", "New York", "Rome"
+            };
+        }
+
+        //create Methods to Send/receive data i.e. send data to controller
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}
