40a489ac357004ac84506921cf64c74bba94da40
Ujjwal Abhishek <ujjwalabhishek15@gmail.com>
2024-09-01 15:12:30 +0530

    21st commit Form Creation using HTML Helpers

---
+++ FormCreationUsingHtmlHelper/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using FormCreationUsingHtmlHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormCreationUsingHtmlHelper.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("user/register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            //list of courses and skills
+            var model = new UserRegistrationModel
+            {
+                Courses = new List<string> { "ASP.NET Core", "Azure", "Microservices" },
+                Skills = new List<string> { "C#", "SQL", "JavaScript", "Docker", "Kubernetes" },
+                HiddenField = Guid.NewGuid()
+            };
+
+            return View(model);
+        }
+
+        [Route("user/register")]
+        [HttpPost]
+        public IActionResult Register(UserRegistrationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //if model is valid return success with model
+                return View("Success", model);
+            }
+
+            //if validation fails, repopulate Course and skill
+            model.Courses = new List<string> { "ASP.NET Core", "Azure", "Microservices" };
+            model.Skills = new List<string> { "C#", "SQL", "JavaScript", "Docker", "Kubernetes" };
+
+            return View(model);
+        }
+    }
+}
---
+++ FormCreationUsingHtmlHelper/Models/UserRegistrationModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormCreationUsingHtmlHelper.Models
+{
+    public class UserRegistrationModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string? Address {  get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public string? Gender { get; set; }
+        public bool RememberMe { get; set; }
+
+        [Required(ErrorMessage = "Please select a course")]
+        public string? SelectedCourse { get; set; }
+        public List<string>? Courses { get; set; }
+
+        [Required(ErrorMessage = "Please select one or more skills")]
+        public List<string>? SelectedSkills { get; set; }
+        public List<string>? Skills { get; set; }
+
+        public Guid HiddenField { get; set; }
+    }
+}
