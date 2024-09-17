21ffe10d7bffb3d76dec5644a0ccce5ed6372233
Ujjwal Abhishek <ujjwalabhishek15@gmail.com>
2024-09-17 15:42:36 +0530

    23 commit File Upload1 in AspNet Core

---
+++ FileUploadAspNetCore1/Controllers/FileUploadController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadAspNetCore1.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        //GET
+        public IActionResult SingleFileUpload()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public async Task<IActionResult> SingleFileUpload(IFormFile SingleFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (SingleFile != null && SingleFile.Length > 0)
+                {
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", SingleFile.FileName);
+                    //Using Buffering
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        // The file is saved in a buffer before being processed
+                        await SingleFile.CopyToAsync(stream);
+                    }
+                    //Using Streaming
+                    //using (var stream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                    //{
+                    //    await SingleFile.CopyToAsync(stream);
+                    //}
+                    // Process the file here (e.g., save to the database, storage, etc.)
+                    return View("UploadSuccess");
+                }
+            }
+            return View("SingleFileUpload");
+        }
+    }
+}
