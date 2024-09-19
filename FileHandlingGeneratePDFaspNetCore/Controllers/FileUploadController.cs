c01a3172c70b9239d49ada0fb36ae76faa2a74a3
Ujjwal Abhishek <ujjwalabhishek15@gmail.com>
2024-09-19 23:07:22 +0530

    27th commit Generate PDF in Asp.NET core MVC

---
+++ FileHandlingGeneratePDFaspNetCore/Controllers/FileUploadController.cs
@@ -0,0 +1,51 @@
+﻿using FileHandlingGeneratePDFaspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileHandlingGeneratePDFaspNetCore.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //generate Invoice PDF using iText
+        public IActionResult GenerateInvoicePDF()
+        {
+            //Sample invoice data
+            //In Real-time you will get the data from the database
+            var invoice = new Invoice
+            {
+                InvoiceNumber = "INV-DOTNET-100",
+                Date = DateTime.Now,
+                CustomerName = "Ujjwal Abhishek",
+                Items = new List<InvoiceItem>
+                {
+                    new InvoiceItem { ItemName = "Item 1", Quantity = 2, UnitPrice = 15.0m },
+                    new InvoiceItem { ItemName = "Item 2", Quantity = 3, UnitPrice = 10.0m },
+                    new InvoiceItem { ItemName = "Item 3", Quantity = 1, UnitPrice = 35.0m }
+                },
+                PaymentMode = "COD"
+            };
+
+            //Sets the Total Amount
+            invoice.TotalAmount = invoice.Items.Sum(x => x.TotalPrice);
+
+            //create object of PDFService
+            PDFService pdfService = new PDFService();
+
+            //Call GeneratePDF() of PDFService passing Invoice Data
+            var pdfFile = pdfService.GeneratePDF(invoice);
+
+            //return PDF File
+            return File(pdfFile, "application/pdf", "Invoice.pdf");
+        }
+
+        [Route("fileupload/download-pdf")]
+        public IActionResult DownloadPDF()
+        {
+            return View("DownloadPDF");
+        }
+    }
+}
---
+++ FileHandlingGeneratePDFaspNetCore/Models/InvoiceModel.cs
@@ -0,0 +1,20 @@
+﻿namespace FileHandlingGeneratePDFaspNetCore.Models
+{
+    public class Invoice
+    {
+        public string InvoiceNumber { get; set; }
+        public DateTime Date { get; set; }
+        public string CustomerName { get; set; }
+        public List<InvoiceItem> Items { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string PaymentMode { get; set; }
+    }
+
+    public class InvoiceItem
+    {
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice => Quantity * UnitPrice;
+    }
+}
